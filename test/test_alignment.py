d412dce0c54210350625266009c3f8686298429a
wesleymarin <wesley.marin@ucsf.edu>
2017-02-24 20:20:13 -0800

    Fixing naming bugs in test script

--- test/test_alignment.py
+++ test/test_alignment.py
@@ -1,4 +1,4 @@
-from alignment_testing
+import alignment_testing as at
 import os
 import matplotlib.pyplot as plt
 import math
@@ -6,16 +6,16 @@ import numpy as np
 
 
 def test_stuff():
-    sequence_list = read_sequences("sequences/")
-    match_matrix = read_match_matrix(".", "BLOSUM50")
-    neg_pairs = read_match_pairs('.', 'Negpairs')
-    pos_pairs = read_match_pairs('.', 'Pospairs')
+    sequence_list = at.read_sequences("sequences/")
+    match_matrix = at.read_match_matrix(".", "BLOSUM50")
+    neg_pairs = at.read_match_pairs('.', 'Negpairs')
+    pos_pairs = at.read_match_pairs('.', 'Pospairs')
 
-    assert type(sequence_list[0]) is Sequence
+    assert type(sequence_list[0]) is at.Sequence
     assert match_matrix['A']['A'] == 5
     assert len(neg_pairs) > 0 and len(neg_pairs) == len(pos_pairs)
 
-    score_table = create_score_table(sequence_list[0], sequence_list[1], match_matrix, -5, -2)
-    match1, match2, score = create_match_strings(score_table, sequence_list[0], sequence_list[1])
+    score_table = at.create_score_table(sequence_list[0], sequence_list[1], match_matrix, -5, -2)
+    match1, match2, score = at.create_match_strings(score_table, sequence_list[0], sequence_list[1])
 
     assert type(score) is np.int64 and score > 0
